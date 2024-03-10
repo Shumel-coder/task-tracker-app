9100f359b69a4ffbba26d99281931205893fa5b6
Muhaimin Ali <muhaiminali@ymail.com>
2024-03-10 16:54:45 -0400

    Implemented form submission handling

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 //Section 1: TODOS
 //TODO: Register submissions from the user on the form.
-//TODO: Determine the value of the data submitted
-//      and add it to a Javascript array called tasks
+//TODO: Determine the value of the data submitted and add it to a Javascript array called tasks
 //TODO: Call the render function to update the table with the new tasks
 
 //Section 2: App State Variables
@@ -10,5 +9,41 @@ let tasks = [];
 //Section 3: Cached Element References
 const taskForm = document.getElementById("taskForm");
 const taskTable = document.getElementById("taskTable");
-console.log(taskForm);
-console.log(taskTable);
+
+//Functions
+//Function to handle form submissions
+function handleSubmission(event) {
+  event.preventDefault();
+  // TODO: Get form input values;
+  const taskName = document.getElementById("taskName");
+  const taskDescription = document.getElementById("taskDescription");
+  const taskDeadline = document.getElementById("taskDeadline");
+  // TODO: Validate input fields;
+  if (
+    document.getElementById("taskName") == "" ||
+    document.getElementById("taskDeadline") == ""
+  ) {
+    return alert("Task name and deadline are required!");
+  } else {
+    return alert(taskName + " has been added");
+  }
+  // TODO: Update the tasks array
+  tasks.push({
+    name: taskName,
+    description: taskDescription,
+    deadline: taskDeadline,
+  });
+  render();
+}
+
+//Function to render tasks in the table
+function render() {
+  // TODO: Use array methods to create a new table row of data for each item in the array.
+}
+
+//Function to initialize the table
+function init() {
+  taskTable.innerHTML = ""; // Clear the table
+  tasks = []; // Reset the tasks array
+  render(); // Call the render function
+}
